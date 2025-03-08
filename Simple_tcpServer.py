1224ca1421daad086c828071c967b6b45aa1ed4e
Gabriel Gutierri <gabrielgutierri04@gmail.com>
2025-03-08 13:04:41 -0300

    correções para envio e recebimento de mensagens + geração de numero aleatorio

--- Simple_tcpServer.py
+++ Simple_tcpServer.py
@@ -1,7 +1,7 @@
 # Código em Python para gerar dois números primos de 2048 bits sem bibliotecas externas
 import random
 from socket import *
-
+import secrets
 #import random
 
 # Lista dos primeiros 1000 primos para filtro rápido de compostos
@@ -24,12 +24,20 @@ SMALL_PRIMES = [
     1979, 1987, 1993, 1997, 1999, 2003, 2011, 2017, 2027, 2029, 2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089,
     2099, 2111, 2113, 2129, 2131, 2137, 2141, 2143, 2153, 2161, 2179, 2203, 2207, 2213, 2221, 2237, 2239, 2243,
     2251, 2267, 2269, 2273, 2281, 2287, 2293, 2297, 2309, 2311, 2333, 2339, 2341, 2347, 2351, 2357, 2371, 2377,
-    2381, 2383, 2389, 2393, 2399, 2411, 2417, 2423, 2437, 2441, 2447, 2459, 2467, 2473, 2477, 2503, 2521, 2531
-] 
+    2381, 2383, 2389, 2393, 2399, 2411, 2417, 2423, 2437, 2441, 2447, 2459, 2467, 2473, 2477, 2503, 2521, 2531,
+    2539, 2543, 2549, 2551, 2557, 2579, 2591, 2593, 2609, 2617, 2621, 2633, 2647, 2657, 2659, 2663, 2671, 2677,
+    2683, 2687, 2689, 2693, 2699, 2707, 2711, 2713, 2719, 2729, 2731, 2741, 2749, 2753, 2767, 2777, 2789, 2791,
+    2797, 2801, 2803, 2819, 2833, 2837, 2843, 2851, 2857, 2861, 2879, 2887, 2897, 2903, 2909, 2917, 2927, 2939,
+    2953, 2957, 2963, 2969, 2971, 2999, 3001, 3011, 3019, 3023, 3037, 3041, 3049, 3061, 3067, 3079, 3083, 3089,
+    3109, 3119, 3121, 3137, 3163, 3167, 3169, 3181, 3187, 3191, 3203, 3209, 3217, 3221, 3229, 3251, 3253, 3257,
+    3259, 3271, 3299, 3301, 3307, 3313, 3319, 3323, 3329, 3331, 3343, 3347, 3359, 3361, 3371, 3373, 3389, 3391,
+    3407, 3413, 3433, 3449, 3457, 3461, 3463, 3467, 3469, 3491, 3499, 3511, 3517, 3527, 3529, 3533, 3539, 3541,
+    3547, 3557, 3559, 3571
+]
 
 def generate_random_4096_bit_number():
     """Gera um número ímpar de 2048 bits que é congruente a 3 mod 4."""
-    num = random.getrandbits(4096) | (1 << 4095) | 1  # Garante 2048 bits e ímpar
+    num = secrets.randbits(4096)| (1 << 4095) | 1  # Garante 2048 bits e ímpar
     return num | 3  # Faz com que seja congruente a 3 mod 4
 
 def is_divisible_by_small_primes(n):
@@ -159,13 +167,19 @@ connectionSocket, addr = serverSocket.accept()
 
 # Geração das chaves públicas e privadas RSA
 server_public_key, server_private_key = generate_rsa_keys()
-
-# Envia a chave pública do servidor para o cliente
-connectionSocket.send(bytes(str(server_public_key), "utf-8"))
+print("Server Público")
+print(server_public_key)
 
 # Recebe a chave pública do cliente
 client_public_key_data = connectionSocket.recv(65000)
 client_public_key = eval(client_public_key_data.decode())  # Converte a chave recebida de volta para tupla (e, N)
+print("Client Público")
+print(client_public_key)
+
+# Envia a chave pública do servidor para o cliente
+connectionSocket.send(bytes(str(server_public_key), "utf-8"))
+
+
 
 # Recebe a mensagem criptografada do cliente
 sentence = connectionSocket.recv(65000)
